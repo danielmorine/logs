26afd9be078ff064feabfeb681a8fcf9fdf54ab0
Daniel Haro <daniel.haro@pixelware.com.br>
2020-06-13 20:50:17 -0300

    .

--- reg/ApplicationDbContext.cs
+++ reg/ApplicationDbContext.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-using Repositories.Interfaces;
+using Repository.Interfaces;
 
 namespace reg
 {
--- reg/Repository/Interfaces/IRepositoryBaseScalar.cs
+++ reg/Repository/Interfaces/IRepositoryBaseScalar.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using reg.Scaffolds.interfaces;
+using System;
 using System.Linq.Expressions;
 using System.Threading.Tasks;
 
