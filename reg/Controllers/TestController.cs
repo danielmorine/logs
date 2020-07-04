40dc22d6dee6e1a5462ec24e80f5c128d3d73429
Daniel Haro <daniel.haro@pixelware.com.br>
2020-07-03 22:32:00 -0300

    test

---
+++ reg/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using reg.Services;
+using System.Threading.Tasks;
+
+namespace reg.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class TestController : ControllerBase
+    {
+        private readonly IGrpcGreeterClient _grpcGreeterClient;
+
+        public TestController(IGrpcGreeterClient grpcGreeterClient)
+        {
+            _grpcGreeterClient = grpcGreeterClient;
+        }
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await _grpcGreeterClient.Say();
+            return Ok();
+        }
+    }
+}
---
+++ reg/Services/GrpcGreeterClient.cs
@@ -0,0 +1,37 @@
+﻿using Grpc.Net.Client;
+using regGRPC;
+using System.Threading.Tasks;
+
+namespace reg.Services
+{
+    public interface IGrpcGreeterClient
+    {
+        Task Say();
+    }
+    public class GrpcGreeterClient : IGrpcGreeterClient
+    {
+        public async Task Say()
+        {
+            // The port number(5001) must match the port of the gRPC server.
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+            //var daniel = reply;
+           
+            var body = Newtonsoft.Json.JsonConvert.SerializeObject( new Body
+            {
+                Name = "Teste", NormalizedName = "TESTE"
+            });
+
+            var reply = await client.SendLevelTypeAsync(new LevelTypeRequest { Name = "Teste", Normalized = "Teste" });
+
+            var daniel = reply;
+        }
+
+        private class Body
+        {
+            public string Name { get; set; }
+            public string NormalizedName { get; set; }
+        }
+    }
+}
