03f326eee2256221eb196472c0133ce789eb4c67
Daniel Haro <daniel.haro@pixelware.com.br>
2020-07-11 19:27:32 -0300

    .

--- reg/Services/GrpcGreeterClient.cs
+++ reg/Services/GrpcGreeterClient.cs
@@ -26,7 +26,7 @@ namespace reg.Services
     }
     public class GrpcGreeterClient : IGrpcGreeterClient
     {
-        private readonly string _url = "http://127.0.0.1:5005";
+        private readonly string _url = "https://localhost:5005";
         private readonly UserManager<ApplicationUser> _userManager;
 
         public GrpcGreeterClient(UserManager<ApplicationUser> userManager)
